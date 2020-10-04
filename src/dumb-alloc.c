ee413ab2b15fd04464bfaab01a354393ce58e0ac
Eric Herman <eric@freesa.org>
2020-10-04 10:00:01 +0200

    doc __STDC_HOSTED__

--- src/dumb-alloc.c
+++ src/dumb-alloc.c
@@ -25,6 +25,30 @@
 #endif /* ARDUINO */
 
 #ifndef DUMB_ALLOC_HOSTED
+/*
+ __STDC_HOSTED__
+ The integer constant 1 if the implementation is a hosted
+ implementation or the integer constant 0 if it is not.
+
+ C99 standard (section 6.10.8):
+ http://www.open-std.org/jtc1/sc22/WG14/www/docs/n1256.pdf
+
+ C++11 standard (section 16.8):
+ http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2012/n3337.pdf
+
+ "The standard also defines two environments for programs, a
+ freestanding environment, required of all implementations and which
+ may not have library facilities beyond those required of
+ freestanding implementations, where the handling of program startup
+ and termination are implementation-defined; and a hosted
+ environment, which is not required, in which all the library
+ facilities are provided and startup is through a function int main
+ (void) or int main (int, char *[]). An OS kernel is an example of a
+ program running in a freestanding environment; a program using the
+ facilities of an operating system is an example of a program
+ running in a hosted environment."
+ https://gcc.gnu.org/onlinedocs/gcc/Standards.html
+*/
 #ifdef __STDC_HOSTED__
 #define DUMB_ALLOC_HOSTED __STDC_HOSTED__
 #else
